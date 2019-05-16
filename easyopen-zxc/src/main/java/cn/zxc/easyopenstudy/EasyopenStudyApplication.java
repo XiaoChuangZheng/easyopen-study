0312ae3c89786983a9cc48a2491f958538447125
zhengxiaochuang <zhengxiaochuang@eefung.com>
2019-05-16 17:54:48 +0800

    feat: 创建一个springboot工程

---
+++ easyopen-zxc/src/main/java/cn/zxc/easyopenstudy/EasyopenStudyApplication.java
@@ -0,0 +1,13 @@
+package cn.zxc.easyopenstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyopenStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyopenStudyApplication.class, args);
+    }
+
+}
